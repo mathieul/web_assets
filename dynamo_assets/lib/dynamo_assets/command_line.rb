6161fe11e994c3184364530dc07665874d0864b2
Mathieu Lajugie <mathieu@caring.com>
2013-12-24 10:21:21 -0800

    switch to seattle style

--- dynamo_assets/lib/dynamo_assets/command_line.rb
+++ dynamo_assets/lib/dynamo_assets/command_line.rb
@@ -6,25 +6,25 @@ module DynamoAssets
 
     attr_reader :arguments
 
-    def initialize(arguments)
+    def initialize arguments
       @arguments = arguments
     end
 
     def parse
       options = {require: [], debug: false}
-      parser(options).parse!(arguments)
+      parser(options).parse! arguments
       options
     end
 
     private
 
-    def parser(options)
+    def parser options
       OptionParser.new do |o|
-        o.on("-r", "--require LIB", "require the library specified") do |lib|
+        o.on "-r", "--require LIB", "require the library specified" do |lib|
           options[:require] << lib
         end
 
-        o.on("-d", "--debug", "trace execution in a log file") do |debug|
+        o.on "-d", "--debug", "trace execution in a log file" do |debug|
           options[:debug] = debug
         end
       end
