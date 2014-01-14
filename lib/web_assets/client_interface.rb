25ca270be28598155dcc06bea1aa6057eba7bfaf
Mathieu Lajugie <mathieu@caring.com>
2014-01-13 18:55:01 -0800

    finish implementing new API in client interface and bump to 0.0.3

--- lib/web_assets/client_interface.rb
+++ lib/web_assets/client_interface.rb
@@ -20,30 +20,54 @@ module WebAssets
     private
 
     def process request
-      request.when [:append_javascript_path, String] do |path|
-        WebAssets.logger.debug "ClientInterface#process: append_javascript_path = #{path.inspect}"
-        reply request, api.append_javascript_path(path)
+      WebAssets.logger.debug "ClientInterface#process: START #{request.inspect}"
+
+      request.when [:set_script_path, String] do |path|
+        WebAssets.logger.debug "ClientInterface#process: set_script_path = #{path.inspect}"
+        reply request, api.set_script_path(path)
       end
 
-      request.when [:append_stylesheet_path, String] do |path|
-        WebAssets.logger.debug "ClientInterface#process: append_stylesheet_path = #{path.inspect}"
-        reply request, api.append_stylesheet_path(path)
+      request.when [:set_stylesheet_path, String] do |path|
+        WebAssets.logger.debug "ClientInterface#process: set_stylesheet_path = #{path.inspect}"
+        reply request, api.set_stylesheet_path(path)
       end
 
-      request.when [:javascript_filenames, String] do |filename|
-        WebAssets.logger.debug "ClientInterface#process: javascript_filenames = #{filename.inspect}"
-        reply request, api.javascript_filenames(filename)
+      request.when [:add_script_load_path, String] do |path|
+        WebAssets.logger.debug "ClientInterface#process: add_script_load_path = #{path.inspect}"
+        reply request, api.add_script_load_path(path)
       end
 
-      request.when [:javascript_content, Array] do |filename, options|
-        WebAssets.logger.debug "ClientInterface#process: javascript_content = #{filename.inspect}"
-        reply request, api.javascript_content(filename, options)
+      request.when [:add_stylesheet_load_path, String] do |path|
+        WebAssets.logger.debug "ClientInterface#process: add_stylesheet_load_path = #{path.inspect}"
+        reply request, api.add_stylesheet_load_path(path)
+      end
+
+      request.when [:script_filenames, String] do |filename|
+        WebAssets.logger.debug "ClientInterface#process: script_filenames = #{filename.inspect}"
+        reply request, api.script_filenames(filename)
+      end
+
+      request.when [:script_digest_filename, String] do |filename|
+        WebAssets.logger.debug "ClientInterface#process: script_digest_filename = #{filename.inspect}"
+        reply request, api.script_digest_filename(filename)
+      end
+
+      request.when [:script_content, Array] do |filename, options|
+        WebAssets.logger.debug "ClientInterface#process: script_content = #{filename.inspect}"
+        reply request, api.script_content(filename, options)
+      end
+
+      request.when [:stylesheet_digest_filename, String] do |filename|
+        WebAssets.logger.debug "ClientInterface#process: stylesheet_digest_filename = #{filename.inspect}"
+        reply request, api.stylesheet_digest_filename(filename)
       end
 
       request.when [:stylesheet_content, Array] do |filename, options|
         WebAssets.logger.debug "ClientInterface#process: stylesheet_content = #{filename.inspect}"
         reply request, api.stylesheet_content(filename, options)
       end
+
+      WebAssets.logger.debug "ClientInterface#process: END"
     end
 
     def reply request, response
